6a5a5c7c23dfbd8ca7c7b8ea87b3df9e456a56ce
Masteralltime <76827615+Masteralltime@users.noreply.github.com>
2024-02-10 13:46:18 -0500

    something dumb

---
+++ titlescreen/infobutton.gd
@@ -0,0 +1,15 @@
+#extends Node
+#
+#var time = 0;
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#time += delta;
+	#$info = 3*sin(time);
+#
+#func _on_info_button_pressed():
+	#
--- titlescreen/titlescreen.gd
+++ titlescreen/titlescreen.gd
@@ -1,16 +1,25 @@
 extends Control
 
 
+
+var time = 0;
+# Called when the node enters the scene tree for the first time.
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	time += delta;
+	$infoButton.scale = Vector2(3*pow(sin(time), 2), 3*pow(sin(time), 2))
 
 
-func _on_button_pressed():
-	
-	get_tree().change_scene("res://player/game.tscn") # Replace with function body.
+
+func _on_play_button_pressed():
+	get_tree().change_scene("res://player/game.tscn")  # Replace with function body.
+
+
+func _on_info_button_pressed():
+	get_tree().change_scene("res://player/info.tscn") # Replace with function body.
