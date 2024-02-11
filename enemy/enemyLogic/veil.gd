5c41488a5670de865c07ba5c472afddd33c00491
Nithya <92411830+mooshroom-person@users.noreply.github.com>
2024-02-11 00:31:23 -0500

    : )
    first commit go brr

---
+++ enemy/enemyLogic/veil.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_collision_polygon_2d_tree_entered():
+	$Character.scale.x(2,2)
+	
+	 # Replace with function body.
